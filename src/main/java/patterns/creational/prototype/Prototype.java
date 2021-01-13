a28e98e3927d2c389d1a08ab9a56c71f9513ca40
Martin Blomberg <martin.blomberg@outlook.com>
2021-01-13 13:40:08 +0100

    Prototype Pattern

---
+++ src/main/java/patterns/creational/prototype/Prototype.java
@@ -0,0 +1,104 @@
+package patterns.creational.prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Adress {
+    String city;
+    String street;
+    int houseNumber;
+
+    public Adress(String city, String street, int houseNumber) {
+        this.city = city;
+        this.street = street;
+        this.houseNumber = houseNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Adress{" +
+                "city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", houseNumber=" + houseNumber +
+                '}';
+    }
+
+    public Adress copy() {
+        return new Adress(this.city,this.street,this.houseNumber);
+    }
+}
+
+class AdressWithCountry extends Adress{
+    String country;
+
+    public AdressWithCountry(String country, String city, String street, int houseNumber) {
+        super(city, street, houseNumber);
+        this.country = country;
+    }
+
+    //If we forget this, copies will be of wrong type. Breaking LSP
+    @Override
+    public Adress copy() {
+        return new AdressWithCountry(this.country,this.city,this.street,this.houseNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "AdressWithCountry{" +
+                "city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", houseNumber=" + houseNumber +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
+
+class Person {
+    String name;
+    int age;
+    Adress adress;  //Reference to an object
+
+    public Person(String name, int age, Adress adress) {
+        this.name = name;
+        this.age = age;
+        this.adress = adress;
+    }
+
+    public Person copy() {
+        //Shallow copy
+        //return new Person(this.name, this.age, this.adress);
+        //Deep copy
+        return new Person(this.name, this.age, this.adress.copy());
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", adress=" + adress +
+                '}';
+    }
+}
+
+public class Prototype {
+
+    public static void main(String[] args) {
+        Person person = new Person("Martin", 43, new AdressWithCountry("Sweden","Kalmar", "Nickebo", 1));
+        System.out.println(person);
+
+        Person person2 = person.copy();
+
+        person2.name = "Kalle";
+        person2.adress.city="Göteborg";
+
+        System.out.println(person);
+        System.out.println(person2);
+
+
+    }
+
+
+
+
+}
